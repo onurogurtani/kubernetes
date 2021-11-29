ff1c53a7c8bea1643d9d49f57d0adda651a96dc5
Onur Oğurtanı <onur.ogurtani@kariyer.net>
2021-11-29 09:40:23 +0300

    nfsServer added

--- vagrant-provisioning/bootstrap.sh
+++ vagrant-provisioning/bootstrap.sh
@@ -55,6 +55,7 @@ echo "export TERM=xterm" >> /etc/bash.bashrc
 
 echo "[TASK 10] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
+172.16.16.110   nfsserver.example.com   nfsserver
 172.16.16.100   kmaster.example.com     kmaster
 172.16.16.101   kworker1.example.com    kworker1
 172.16.16.102   kworker2.example.com    kworker2
